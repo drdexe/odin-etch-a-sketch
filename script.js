063c8446c56ec0eb72495622d10845c3beca6938
drdexe <104161856+drdexe@users.noreply.github.com>
2025-04-24 18:19:25 +0800

    Add button functionality

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+const button = document.querySelector("button");
 const container = document.querySelector(".container");
-for (let i = 0; i < 16 * 16; i++) {
-  const div = document.createElement("div");
-  div.addEventListener("mouseenter", () => {
-    div.style.backgroundColor = "black";
-  });
-  container.appendChild(div);
+
+function createGrid(n=16) {
+  for (let i = 0; i < n ** 2; i++) {
+    const div = document.createElement("div");
+    div.style.width = div.style.height = `${100 / n}%`;
+    div.addEventListener("mouseenter", () => {
+      div.style.backgroundColor = "black";
+    });
+    container.appendChild(div);
+  }
 }
+
+function deleteGrid() {
+  document.querySelectorAll(".container > div").forEach(div => {
+    div.remove();
+  });
+}
+
+createGrid();
+
+button.addEventListener("click", () => {
+  const num = parseInt(prompt("Number of squares per side: (1-100)"));
+  if (isNaN(num) || num <= 0 || num > 100) {
+    alert("Invalid input!");
+  } else {
+    deleteGrid();
+    createGrid(num);
+  }
+});
