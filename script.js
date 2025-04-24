2274f75cced2243e9a031150bf0277f1462ec1b3
drdexe <104161856+drdexe@users.noreply.github.com>
2025-04-24 17:31:58 +0800

    setup hover effect on divs

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const container = document.querySelector(".container");
 for (let i = 0; i < 16 * 16; i++) {
   const div = document.createElement("div");
+  div.addEventListener("mouseenter", () => {
+    div.style.backgroundColor = "black";
+  });
   container.appendChild(div);
 }
